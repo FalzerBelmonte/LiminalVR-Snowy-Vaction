f252078d69c04a1044aa96f5549f0b3935bbbb2f
Alma <willalma123@gmail.com>
2020-11-29 03:33:25 +1100

    Added Candles, Rug, Lamp, windows and doors. Snow particles and flames.
    Added one script for turning off and on candle lights.

---
+++ Assets/Tasty Gamers/Scripts/Candles/CandleLights.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleLights : MonoBehaviour
+{
+
+    public GameObject Flames;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Making sure that the flames are set to true.
+        Flames.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Turning off and on candles' flames.
+    public void OffnOnFlames()
+    {
+        if(Flames.activeSelf == true)
+        {
+            Flames.SetActive(false);
+        }else
+        if (Flames.activeSelf == false)
+        {
+            Flames.SetActive(true);
+        }
+
+    }
+
+}
