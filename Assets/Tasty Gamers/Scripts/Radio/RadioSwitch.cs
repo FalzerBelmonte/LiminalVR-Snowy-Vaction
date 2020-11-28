2b160a984de9ad0080ca2e9a8af4418ab522b17e
Alma <willalma123@gmail.com>
2020-11-29 00:21:46 +1100

    Liminal VR

---
+++ Assets/Tasty Gamers/Scripts/Radio/RadioSwitch.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioSwitch : MonoBehaviour
+{
+    public List<AudioClip> Songs;
+    private AudioSource sPlayer;
+    public int CurrentSong;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            Transform tChild = gameObject.transform.GetChild(i);
+            if(tChild.GetComponent<AudioSource>())
+            {
+                sPlayer = tChild.GetComponent<AudioSource>();
+                sPlayer.clip = Songs[0];
+                sPlayer.Play();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToggleUp()
+    {
+        ++CurrentSong;
+        if(CurrentSong < Songs.Count)
+        {
+            sPlayer.clip = Songs[CurrentSong];
+            sPlayer.Play();
+        }
+        else
+        {
+            CurrentSong = 0;
+            sPlayer.clip = Songs[0];
+            sPlayer.Play();
+        }
+    }
+
+    public void ToggleDown()
+    {
+        --CurrentSong;
+        if(CurrentSong < 0)
+        {
+            CurrentSong = Songs.Count - 1;
+            sPlayer.clip = Songs[CurrentSong];
+            sPlayer.Play();
+        }
+        else
+        {
+            sPlayer.clip = Songs[CurrentSong];
+            sPlayer.Play();
+        }
+    }
+}
